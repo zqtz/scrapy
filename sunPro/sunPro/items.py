6e756efa28ee1cb1ac0e0e78ae3f727adaa74cdd
sunny <61925624+zqtz@users.noreply.github.com>
2022-02-18 11:30:52 +0800

    Update items.py

--- sunPro/sunPro/items.py
+++ sunPro/sunPro/items.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Define here the models for your scraped items
 #
 # See documentation in:
@@ -7,12 +9,14 @@ import scrapy
 
 
 class SunproItem(scrapy.Item):
+    sun_id = scrapy.Field()
+    sun_title = scrapy.Field()
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    def sunItem(self):
-        sun_id = scrapy.Field()
-        sun_title = scrapy.Field()
 
-    def detailItem(self):
-        detail_id = scrapy.Field()
-        detail_content = scrapy.Field()
+
+class DetailItem(scrapy.Item):
+    detail_id = scrapy.Field()
+    detail_content = scrapy.Field()
+
+
+
